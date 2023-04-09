6c13cce04d72bd6135cf2e50d3ded71d1ca978f1
Ayushi0516 <ayushigupta0516@gmail.com>
2023-04-09 18:08:21 +0530

    package added

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+ const express=require("express")
+
+ const app=express()
+
+  app.get("/",(req,res)=>{
+        res.send("Homepage")
+    })
+
+
+app.listen("7000",()=>{
+    console.log("running on 7000")
+})
+
+
