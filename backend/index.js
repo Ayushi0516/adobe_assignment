d7fd20c29e9df382b15ccc904d350d02dac26491
Ayushi0516 <ayushigupta0516@gmail.com>
2023-04-09 22:05:59 +0530

    models added

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
  const express=require("express")
+ const {connection}=require("./config/db")
+ const cors = require("cors")
+ require('dotenv').config()
 
+const PORT=process.env.PORT ||8080;
  const app=express()
+ app.use(express.json())
 
   app.get("/",(req,res)=>{
         res.send("Homepage")
     })
+    app.use(cors())
+    
 
-
-app.listen("7000",()=>{
-    console.log("running on 7000")
-})
-
-
+    app.listen(PORT,async()=>{
+        try{
+            await connection;
+            console.log("Connected to db")
+        }
+        catch(err){
+            console.log("Error connnecting to DB")
+            console.log(err)
+        }
+        console.log(`listning on ${PORT}`)
+    })
---
+++ backend/model/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: mongoose.Types.ObjectId, required: true, auto: true },
+    name: { type: String, required: true, maxlength: 50 },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+    },
+    bio: { type: String, maxlength: 200 },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+};
